24973a6e392388498c98b8e9e773083b42f3c23c
s69073584-oss <s69073584@gmail.com>
2026-08-25 13:53:02 +0500

    First Commit

---
+++ app/api/inngest/route.js
@@ -0,0 +1,11 @@
+import { serve } from "inngest/next";
+import { inngest,  syncUserCreation, syncUserUpdate, syncUserDeletion } from "@/config/inngest";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdate,
+    syncUserDeletion
+  ],
+});
